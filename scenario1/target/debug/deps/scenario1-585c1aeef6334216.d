429ffd7904f9cfc1b2737fb0952308c9343214a6
zakie <realzzaekkii@gmail.com>
2024-07-05 21:38:54 +0900

    Create workspace for TCP/HTTP server, client

---
+++ scenario1/target/debug/deps/scenario1-585c1aeef6334216.d
@@ -0,0 +1,5 @@
+D:\Rust\EzyTutors\scenario1\target\debug\deps\libscenario1-585c1aeef6334216.rmeta: src\main.rs
+
+D:\Rust\EzyTutors\scenario1\target\debug\deps\scenario1-585c1aeef6334216.d: src\main.rs
+
+src\main.rs:

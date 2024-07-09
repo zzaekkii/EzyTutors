0115e9ead4c5d90306729256405dc941ab53ca63
zakie <realzzaekkii@gmail.com>
2024-07-09 18:16:42 +0900

    Update TCP server to echo server

--- scenario1/tcpserver/src/main.rs
+++ scenario1/tcpserver/src/main.rs
@@ -1,10 +1,14 @@
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
     let connection_listner = TcpListener::bind("127.0.0.1:3000").unwrap();
     println!("Running on port 3000");
     for stream in connection_listner.incoming() {
-        let _stream = stream.unwrap();
+        let mut _stream = stream.unwrap();
         println!("Connection established");
+        let mut buffer = [0; 1024];
+        _stream.read(&mut buffer).unwrap();
+        _stream.write(&mut buffer).unwrap();
     }
 }

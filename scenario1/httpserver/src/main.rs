c45c136178d0e5fdc09c207c18913dc99a47bb4b
zakie <realzzaekkii@gmail.com>
2024-07-22 16:24:20 +0900

    Add main func for httpserver

--- scenario1/httpserver/src/main.rs
+++ scenario1/httpserver/src/main.rs
@@ -1,3 +1,12 @@
+mod handler;
+mod server;
+mod router;
+use server::Server;
+
 fn main() {
-    println!("Hello, world!");
+    // 서버 가동.
+    let server = Server::new("localhost:3000");
+
+    // 서버 실행.
+    server.run();
 }

becd0b2f03ce3164c1276c57cdd92593ac4f24b6
zakie <realzzaekkii@gmail.com>
2024-09-22 20:49:06 +0900

    Fix content ch4 to ch5

---
+++ ezytutors/tutor-db/src/bin/iter4.rs
@@ -0,0 +1,48 @@
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use sqlx::postgres::PgPool;
+use std::env;
+use std::io;
+use std::sync::Mutex;
+
+#[path = "../iter4/db_access.rs"]
+mod db_access;
+#[path = "../iter4/errors.rs"]
+mod errors;
+#[path = "../iter4/handlers.rs"]
+mod handlers;
+#[path = "../iter4/models.rs"]
+mod models;
+#[path = "../iter4/routes.rs"]
+mod routes;
+#[path = "../iter4/state.rs"]
+mod state;
+
+use routes::*;
+use state::AppState;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect(
+        "DATABASE_URL is not set in .env file");
+    let dp_pool = PgPool::connect(&database_url).await.unwrap();
+
+    let shared_data = web::Data::new(AppState {
+        health_check_response: "I'm stay'in alive. You've already asked me ".to_string(),
+        visit_count: Mutex::new(0),
+        db: dp_pool,
+    });
+
+    let app = move || {
+        App::new()
+            .app_data(shared_data.clone()) // app 상태 인스턴스에 주입
+            .configure(general_routes)
+            .configure(course_routes)
+    };
+
+    let host_port = env::var("HOST_PORT").expect(
+        "HOST:PORT address is not set in .env file");
+    HttpServer::new(app).bind(&host_port)?.run().await
+}
--- ezytutors/tutor-db/src/database.sql
+++ ezytutors/tutor-db/src/database.sql
@@ -1,8 +1,8 @@
 /* 테이블이 존재하면 삭제 */
-drop table if exists ezy_course_ch4;
+drop table if exists ezy_course_ch5;
 
 /* 테이블 생성 */
-create table ezy_course_ch4
+create table ezy_course_ch5
 (
     course_id serial primary key,
     tutor_id INT not null,
@@ -12,8 +12,8 @@ create table ezy_course_ch4
 
 
 /* 테스트용 시드 데이터 로드 */
-insert into ezy_course_ch4 (course_id, tutor_id, course_name, posted_time)
-values (1, 1, '스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술', '2024-08-27 14:09:08');
+insert into ezy_course_ch5 (course_id, tutor_id, course_name, posted_time)
+values (1, 1, '스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술', '2024-09-06 21:07:08');
 
-insert into ezy_course_ch4 (course_id, tutor_id, course_name, posted_time)
-values (2, 1, '스프링 DB 1편 - 데이터 접근 핵심 원리', '2024-08-27 14:10:52');
+insert into ezy_course_ch5 (course_id, tutor_id, course_name, posted_time)
+values (2, 1, '스프링 DB 1편 - 데이터 접근 핵심 원리', '2024-09-06 14:07:17');
